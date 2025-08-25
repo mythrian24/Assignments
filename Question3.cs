2abbfeff6fa595ae06ddc41eb7a429ce6c7ab00b
mythrian24 <mythrian24@gmail.com>
2025-08-25 12:09:49 +0530

    Add files via upload

---
+++ Question3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Question3
+{
+    enum Days { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+
+    static void Main(string[] args)
+    {
+        int[] units = new int[7];
+
+        Console.WriteLine("Enter units consumed for each day (Mon to Sun):");
+        for (int i = 0; i < 7; i++)
+        {
+            Console.Write("Day " + (i + 1) + ": ");
+            units[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int total = 0;
+        int maxUnits = units[0];
+        int maxDayIndex = 0;
+
+        for (int i = 0; i < 7; i++)
+        {
+            total += units[i];
+            if (units[i] > maxUnits)
+            {
+                maxUnits = units[i];
+                maxDayIndex = i;
+            }
+        }
+
+        double average = (double)total / 7;
+
+        Console.WriteLine("\nTotal units: " + total);
+        Console.WriteLine("Average units/day: " + average.ToString("0.00"));
+        Console.WriteLine("Highest consumption: " + (Days)maxDayIndex + " (" + maxUnits + " units)");
+    }
+}
---
+++ question2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Customer
+{
+    public int CustomerID;
+    public string Name;
+    public int UnitsConsumed;
+
+    public Customer(int customerID, string name, int unitsConsumed)
+    {
+        CustomerID = customerID;
+        Name = name;
+        UnitsConsumed = unitsConsumed;
+    }
+
+    public void ShowBill()
+    {
+        int billAmount = UnitsConsumed * 5; 
+        Console.WriteLine("\nCustomer: " + Name + " (ID: " + CustomerID + ")");
+        Console.WriteLine("Units Consumed: " + UnitsConsumed);
+        Console.WriteLine("Total Bill: $" + billAmount);
+    }
+}
+
+class Program
+{
+    //static void Main(string[] args)
+    //{
+    //    Console.Write("Enter Customer ID: ");
+    //    int id = Convert.ToInt32(Console.ReadLine());
+
+    //    Console.Write("Enter Name: ");
+    //    string name = Console.ReadLine();
+
+    //    Console.Write("Enter Units Consumed: ");
+    //    int units = Convert.ToInt32(Console.ReadLine());
+
+    //    Customer customer = new Customer(id, name, units);
+    //    customer.ShowBill();
+    //}
+}
