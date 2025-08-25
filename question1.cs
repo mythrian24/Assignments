e36308b490e5c0a38a32a0191b9b0bd7e157e964
mythrian24 <mythrian24@gmail.com>
2025-08-25 12:14:31 +0530

    Update question1.cs

--- question1.cs
+++ question1.cs
@@ -1,17 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-//class Smart_meter {
-//    static void Main() {
-//        Console.WriteLine("Enter units consumed:");
-//        int units = Convert.ToInt32(Console.ReadLine());
-//        int Total_bill=0;
-//        if (units < 100)
-//            Total_bill = units * 5;
-//        else if (units > 100 && units < 200)
-//            Total_bill = 100 * 5 + (units - 100) * 7;
-//        else
-//            Total_bill = 100 * 5 + 100 * 7 + (units - 200) * 10;
-//        Console.WriteLine("Total electricity bill: " + Total_bill);
-//    }
-//}
+class Smart_meter {
+   static void Main() {
+       Console.WriteLine("Enter units consumed:");
+       int units = Convert.ToInt32(Console.ReadLine());
+       int Total_bill=0;
+       if (units < 100)
+           Total_bill = units * 5;
+       else if (units > 100 && units < 200)
+           Total_bill = 100 * 5 + (units - 100) * 7;
+       else
+           Total_bill = 100 * 5 + 100 * 7 + (units - 200) * 10;
+       Console.WriteLine("Total electricity bill: " + Total_bill);
+   }
+}
+
