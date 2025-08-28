3f8243e94c40c80f54d521f65e2bd782d0cc6e68
mythrian24 <mythrian24@gmail.com>
2025-08-28 12:04:19 +0530

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+abstract class MeterReading
+{
+    public abstract int CalculateBill(int units);
+}
+
+
+class ResidentialReading : MeterReading
+{
+    public override int CalculateBill(int units)
+    {
+        return units * 5;
+    }
+}
+
+
+class CommercialReading : MeterReading
+{
+    public override int CalculateBill(int units)
+    {
+        return units * 8;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MeterReading residential = new ResidentialReading();
+        MeterReading commercial = new CommercialReading();
+
+        int resUnits = 100;
+        int comUnits = 100;
+
+        int resBill = residential.CalculateBill(resUnits);
+        int comBill = commercial.CalculateBill(comUnits);
+
+        Console.WriteLine("Residential Bill = " + resBill); 
+        Console.WriteLine("Commercial Bill = " + comBill);    
+    }
+}
+
