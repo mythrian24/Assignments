b137fe6710dbbfab1ed3f55f3563e386e0ba61c0
mythrian24 <mythrian24@gmail.com>
2025-08-28 11:58:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class SmartMeter
+{
+    public int MeterId;
+    public string CustomerName;
+
+    public SmartMeter(int meterId, string customerName)
+    {
+        MeterId = meterId;
+        CustomerName = customerName;
+    }
+}
+
+class ResidentialMeter : SmartMeter
+{
+    public string HouseType;
+
+    public ResidentialMeter(int meterId, string customerName, string houseType)
+        : base(meterId, customerName)
+    {
+        HouseType = houseType;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine("Residential Meter -> ID: " + MeterId + ", Name: " + CustomerName + ", HouseType: " + HouseType);
+    }
+}
+
+class CommercialMeter : SmartMeter
+{
+    public string BusinessType;
+
+    public CommercialMeter(int meterId, string customerName, string businessType)
+        : base(meterId, customerName)
+    {
+        BusinessType = businessType;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine("Commercial Meter -> ID: " + MeterId + ", Name: " + CustomerName + ", BusinessType: " + BusinessType);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ResidentialMeter resMeter = new ResidentialMeter(201, "Alice", "Apartment");
+        CommercialMeter comMeter = new CommercialMeter(301, "Bob", "Shop");
+
+        resMeter.PrintDetails();
+        comMeter.PrintDetails();
+    }
+}
+
